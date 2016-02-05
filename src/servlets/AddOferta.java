501f9204d0f4f5fb11e01da7b70e4517f2bfdb22
carlo <carlo@192.168.1.4>
2016-02-05 19:01:11 -0200

    ofertaADD

--- src/servlets/AddOferta.java
+++ src/servlets/AddOferta.java
@@ -1,11 +1,23 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.FabricaConexao;
+import DAO.OfertaDeCompraDAO;
+
+import modelo.Comentario;
+import modelo.OfertaDeCompra;
+import modelo.Usuario;
+import excecao.FalhaNoBanco;
 
 /**
  * Servlet implementation class addOferta
@@ -13,12 +25,15 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/addOferta")
 public class AddOferta extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private OfertaDeCompraDAO ofertaDAO;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public AddOferta() {
         super();
+		Connection conexao = new FabricaConexao().getConnection();
+        ofertaDAO =  new OfertaDeCompraDAO(conexao);
         // TODO Auto-generated constructor stub
     }
 
@@ -33,7 +48,30 @@ public class AddOferta extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String classificado = request.getParameter("idClassificado");
+		try {
+			String conteudo = request.getParameter("conteudo");
+			long idClassificado = Long.parseLong(classificado);
+			
+			HttpSession session = request.getSession();
+			Usuario usuario = (Usuario) session.getAttribute("usuario");
+
+			OfertaDeCompra oferta = new OfertaDeCompra();
+			oferta.setIdClassificado(idClassificado);
+			oferta.setConteudo(conteudo);
+			oferta.setUsuario(usuario);
+			this.ofertaDAO.cadastrar(oferta);
+			response.sendRedirect("listarClassificados");
+			return;
+		} catch (RuntimeException e) {
+			request.setAttribute("erro_add_oferta", "Informações Inválida!");
+		} catch (FalhaNoBanco e) {
+			request.setAttribute("erro_add_oferta",
+					"Ocorreu alguma falha no banco!");
+		}
+		RequestDispatcher rq = request
+				.getRequestDispatcher("listarNoticias");
+		rq.forward(request, response);
 	}
 
 }
