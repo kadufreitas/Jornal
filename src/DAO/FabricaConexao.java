74b2bc4e109a296d8c25a609ae8e760901c6bbec
carlo <carlo@192.168.1.4>
2016-02-05 05:36:44 -0200

    Fabric

---
+++ src/DAO/FabricaConexao.java
@@ -0,0 +1,17 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+	public Connection getConnection(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");//alterar tabela
+			return DriverManager.getConnection("jdbc:mysql://localhost/lolnews","root","root");
+		} catch (ClassNotFoundException|SQLException e) {
+			System.out.println(e.getMessage());
+			throw new RuntimeException();
+		}
+	}
+}
--- src/DAO/UsuarioDAO.java
+++ src/DAO/UsuarioDAO.java
@@ -1,17 +1,58 @@
 package DAO;
 
 import modelo.Usuario;
+import java.sql.*;
 
 public class UsuarioDAO {
+	private Connection connection;
+	
 	public UsuarioDAO(){
-		
+		this.connection = new FabricaConexao().getConnection();
 	}
 	
 	public void cadastrar(Usuario usuario){
 		
+		 String sql = "insert into usuario " +
+				  "(nome,email,senha)" +
+				  " values (?,?,?)";
+				 
+				  try {
+				  PreparedStatement stmt = connection.prepareStatement(sql);
+				 
+				  // seta os valores
+				  stmt.setString(1,usuario.getNome());
+				  stmt.setString(2,usuario.getEmail());
+				  stmt.setString(3,usuario.getSenha());
+				  
+				  // executa
+				  stmt.execute();
+				  stmt.close();
+				  } catch (SQLException e) {
+					  throw new RuntimeException(e);
+				  }
 	}
 		
-	public Usuario pegarPorId(int idUsuario){
+	public Usuario pegarPorId(long idUsuario){
+		PreparedStatement stmt = null;
+		ResultSet rs = null;	
+		try {
+			stmt = connection.prepareStatement("Select * from admin where id=?");
+			stmt.setLong(1, idUsuario);
+			rs = stmt.executeQuery();
+			Usuario usuario = new Usuario();
+			
+			if(rs.first()){
+				usuario.setEmail(rs.getString("email"));
+				usuario.setId(rs.getInt("id"));
+				usuario.setNome(rs.getString("nome"));
+				return usuario;
+			}
+			
+			return null;
+		} catch (SQLException e) {
+			//throw new ErroNoDao();
+		} 
 		return null;
 	}
+	
 }
